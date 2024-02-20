85bb59951b6a773d11af6eae917a91f0465ced55
abhaykumar-0285 <abhaykumarghy2005@gmail.com>
2024-02-20 21:24:16 +0530

    done

--- function/fun10.c
+++ function/fun10.c
@@ -3,9 +3,9 @@
 #include<stdio.h>
 #include<conio.h>
 #include<math.h>
-int sum(int a,int b){
+int sum(int a,int b){//formal parameter
     int c = a+b;
-    printf("sum of two number: %d",c);
+    //printf("sum of two number: %d",c);
     return c;
 }
 int main()
@@ -15,6 +15,8 @@ int main()
  scanf("%d",&a);
  printf("enter the second number: ");
  scanf("%d",&b);
- sum(a,b);
+ int c = sum(a,b);
+ //where as sum(2,4) this are call actual parameter
+ printf("sum of two numbers: %d",c);
  return 0;
 }
