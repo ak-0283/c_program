7ab12d0b132c86e468c6a177bc29e72d5fc4eb60
abhaykumar-0285 <abhaykumarghy2005@gmail.com>
2024-02-23 22:13:59 +0530

    done

---
+++ DSA/fun1.c
@@ -0,0 +1,38 @@
+// linear search without function
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+ int a[100],n,s;
+ printf("enter the size of array: ");
+ scanf("%d",&n);
+ printf("enter the array elements: \n");
+ for (int i = 0; i < n; i++)
+ {
+    scanf("%d",&a[i]);
+ }
+ printf("array elements is: ");
+ for (int i = 0; i < n; i++)
+ {
+        printf("%d \n",a[i]);
+    
+ }
+ printf("enter the elements which is going to search: ");
+ scanf("%d",&s);
+ for (int i = 0; i < n; i++)
+ {
+    if (a[i]==s)        
+    {
+        printf("the elements is present %d at location %d",s,i);
+        break;
+    }
+    else if(a[i]==n)
+    {
+    printf("the elements is not present");
+    break;
+    }
+ }
+ return 0;
+}
---
+++ DSA/fun3.c
@@ -0,0 +1,43 @@
+//binary search without using function
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+int main()
+{
+ int i,n,s,beg,end,mid,a[100];
+ printf("enter the size of array: ");
+ scanf("%d",&n);
+ printf("enter array elements: ");
+ for ( i = 0; i < n; i++)
+ {
+    scanf("%d",&a[i]);
+ }
+ printf("enter the search elements: ");
+ scanf("%d",&s);
+ beg=0;
+ end=n-1;
+ mid=(beg+end)/2;
+ while(beg<=end)
+ {
+    if(a[mid]==s)
+    {
+    printf("%d found at location %d\n",s,mid);
+    break;
+    }
+    else if(a[mid<s])
+    {
+        beg=mid+1;
+    }
+    else
+    {
+        end=mid-1;
+    }
+ }
+ if(beg>end)
+ {
+    printf("not found %d is not present in the list",s);
+ }
+ return 0;
+}
